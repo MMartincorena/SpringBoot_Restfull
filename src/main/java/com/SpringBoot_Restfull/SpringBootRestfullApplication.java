3d0d8a46d719bbd4f5b3edb5fbc734c5df40aa74
MMartincorena <martinmartincorena@gmail.com>
2022-02-17 23:40:51 -0300

    inicio de repositorio

---
+++ src/main/java/com/SpringBoot_Restfull/SpringBootRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_Restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfullApplication.class, args);
+	}
+
+}
